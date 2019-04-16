11523526533d795dccab425577dae69212c7be3e
Marco Pennekamp <marco.pennekamp96@gmail.com>
2019-04-16 02:24:16 +0200

    Implement raw type builder import tests.

--- generator/src/test/scala/com/github/choppythelumberjack/trivialgen/test/RawTypeBuilderSpec.scala
+++ generator/src/test/scala/com/github/choppythelumberjack/trivialgen/test/RawTypeBuilderSpec.scala
@@ -1,6 +1,9 @@
 package com.github.choppythelumberjack.trivialgen.test
 
+import java.time.{LocalDate, LocalDateTime}
+
 import com.github.choppythelumberjack.trivialgen.generator.{Import, ImportSimplifyingRawTypeBuilder}
+import com.github.choppythelumberjack.trivialgen.test.RawTypeBuilderSpec.traits.Trait
 import com.github.choppythelumberjack.trivialgen.test.RawTypeBuilderSpec.{CaseClass, ParamAlias, SimpleAlias}
 import org.scalatest.{FlatSpec, Matchers}
 
@@ -10,12 +13,16 @@ object RawTypeBuilderSpec {
   type SimpleAlias = Boolean
   type ParamAlias[A, B] = Map[A, B]
   case class CaseClass()
+
+  object traits {
+    trait Trait
+  }
 }
 
 class RawTypeBuilderSpec extends FlatSpec with Matchers {
 
   private def basicBuilder() = new ImportSimplifyingRawTypeBuilder(Set(
-    Import.Package("com.github.choppythelumberjack.trivialgen.test.RawTypeBuilderSpec")
+    Import.Package("com.github.choppythelumberjack.trivialgen.test.RawTypeBuilderSpec"),
   ))
 
   "ImportSimplifyingRawTypeBuilder" should "build primitive types correctly" in {
@@ -47,7 +54,18 @@ class RawTypeBuilderSpec extends FlatSpec with Matchers {
   }
 
   it should "simplify imported names correctly" in {
-    // TODO: Implement
+    val builder = new ImportSimplifyingRawTypeBuilder(Set(
+      Import.Package("com.github.choppythelumberjack.trivialgen.test.RawTypeBuilderSpec"),
+      Import.Entity(typeOf[LocalDateTime]),
+    ))
+
+    // CaseClass should be simplified fully while Trait should be simplified up to the traits object.
+    builder(typeOf[CaseClass]) shouldEqual "CaseClass"
+    builder(typeOf[Trait]) shouldEqual "traits.Trait"
+
+    // Only LocalDateTime has been imported as an entity, so LocalDate should need to be fully qualified.
+    builder(typeOf[LocalDateTime]) shouldEqual "LocalDateTime"
+    builder(typeOf[LocalDate]) shouldEqual "java.time.LocalDate"
   }
 
 }
