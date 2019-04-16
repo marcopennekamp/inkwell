188527cc4a7369f9ccc3bfc65c35600f2160ccf6
Marco Pennekamp <marco.pennekamp96@gmail.com>
2019-04-16 02:21:31 +0200

    Remove unused utils.

--- generator/src/main/scala/com/github/choppythelumberjack/trivialgen/util/StringSeqUtil.scala
+++
@@ -1,7 +0,0 @@
-package com.github.choppythelumberjack.trivialgen.util
-
-object StringSeqUtil {
-  implicit class StringSeqExt(seq:Seq[String]) {
-    def pruneEmpty = seq.filterNot(_.trim == "")
-  }
-}
