239830e4722b1782c6c4ebc6bee2eee40e56680f
Marco Pennekamp <marco.pennekamp96@gmail.com>
2019-04-16 23:59:07 +0200

    Has to be years, of course.

--- integration-tests/src/main/scala/plumbus/academy/PersonFunctions.scala
+++ integration-tests/src/main/scala/plumbus/academy/PersonFunctions.scala
@@ -4,5 +4,5 @@ import java.time.LocalDateTime
 import java.time.temporal.ChronoUnit
 
 trait PersonFunctions { self: Person =>
-  def age: Long = self.birthday.until(LocalDateTime.now(), ChronoUnit.DAYS)
+  def age: Long = self.birthday.until(LocalDateTime.now(), ChronoUnit.YEARS)
 }
